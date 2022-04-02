343d335dd83f01bf20ec703e3ba77131d917eab2
Pai Peng <sipaipv6@gmail.com>
2022-04-02 15:48:22 +0800

    add security and jpa/hibernate dependencies

--- src/main/java/com/paipeng/saas/Application.java
+++ src/main/java/com/paipeng/saas/Application.java
@@ -3,12 +3,15 @@ package com.paipeng.saas;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
-@SpringBootApplication
+//@SpringBootApplication
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
     //private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
