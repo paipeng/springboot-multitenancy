efdd8edb8e19fe7e5cbb09f2bbaf18fdbdfeb780
Pai Peng <sipaipv6@gmail.com>
2022-04-03 10:46:38 +0800

    add annotation @Repository

--- src/main/java/com/paipeng/saas/repository/UserRepository.java
+++ src/main/java/com/paipeng/saas/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.paipeng.saas.repository;
 import com.paipeng.saas.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
