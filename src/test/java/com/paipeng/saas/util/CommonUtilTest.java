cff185e5b9f48f9e1d51ab31cd239fb1c3ef0053
Pai Peng <sipaipv6@gmail.com>
2022-04-06 17:05:41 +0800

    update unit test

--- src/test/java/com/paipeng/saas/util/CommonUtilTest.java
+++ src/test/java/com/paipeng/saas/util/CommonUtilTest.java
@@ -5,10 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonUtilTest {
+    private static final String JWT_SECRET = "5161127a80ff47a1855176c345a1de39833b486ea3dd40629081ab0370a1632c87496492fb634c60a458182c69a7f0d0";
 
     @org.junit.jupiter.api.Test
     void generateJWTToken() {
-        String token = CommonUtil.generateJWTToken("5161127a80ff47a1855176c345a1de39833b486ea3dd40629081ab0370a1632c87496492fb634c60a458182c69a7f0d0", "paipeng");
+        String token = CommonUtil.generateJWTToken(JWT_SECRET, "paipeng", "tenant_1");
         System.out.println("token: " + token);
     }
+
+    @org.junit.jupiter.api.Test
+    void generateJWTToken2() {
+        String token = CommonUtil.generateJWTToken(JWT_SECRET, "pai", "tenant_2");
+        System.out.println("token: " + token);
+    }
+
 }
