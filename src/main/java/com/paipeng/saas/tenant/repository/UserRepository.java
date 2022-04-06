d165a8ba42564d8f0f36ec7f78946a2af8147d13
Pai Peng <sipaipv6@gmail.com>
2022-04-06 14:38:46 +0800

    findByToken

--- src/main/java/com/paipeng/saas/tenant/repository/UserRepository.java
+++ src/main/java/com/paipeng/saas/tenant/repository/UserRepository.java
@@ -46,4 +46,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select p from User p where p.username = :username and p.tenant = :tenant")
     User findByUsernameAndTenantname(@Param("username") String username,
             @Param("tenant") String tenant);
+
+
+    User findByToken(String token);
 }
