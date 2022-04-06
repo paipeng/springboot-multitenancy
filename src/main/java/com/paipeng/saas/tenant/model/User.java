e90746fd5d6287bf73bee6afec71e158d93e031a
Pai Peng <sipaipv6@gmail.com>
2022-04-06 20:37:37 +0800

    Revert "set to lazy"
    This reverts commit d34c9de51111bcb06661e5c65135879d988e7e7e.

--- src/main/java/com/paipeng/saas/tenant/model/User.java
+++ src/main/java/com/paipeng/saas/tenant/model/User.java
@@ -91,7 +91,7 @@ public class User implements Serializable {
      * and vice versa
      */
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
