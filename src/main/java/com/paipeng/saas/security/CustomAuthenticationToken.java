425950f73e694cd5e8b7be3711d77965b337acc5
Pai Peng <sipaipv6@gmail.com>
2022-04-06 10:09:55 +0800

    add log

--- src/main/java/com/paipeng/saas/security/CustomAuthenticationToken.java
+++ src/main/java/com/paipeng/saas/security/CustomAuthenticationToken.java
@@ -1,11 +1,14 @@
 package com.paipeng.saas.security;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
 public class CustomAuthenticationToken extends UsernamePasswordAuthenticationToken {
+    private final static Logger logger = LogManager.getLogger(CustomAuthenticationToken.class.getSimpleName());
 
     private static final long serialVersionUID = 1L;
 
@@ -21,7 +24,9 @@ public class CustomAuthenticationToken extends UsernamePasswordAuthenticationTok
      */
     public CustomAuthenticationToken(Object principal, Object credentials,
                                      String tenant) {
+
         super(principal, credentials);
+        logger.info("CustomAuthenticationToken: " + tenant);
         this.tenant = tenant;
         super.setAuthenticated(false);
     }
@@ -35,6 +40,8 @@ public class CustomAuthenticationToken extends UsernamePasswordAuthenticationTok
     public CustomAuthenticationToken(Object principal, Object credentials,
                                      String tenant, Collection<? extends GrantedAuthority> authorities) {
         super(principal, credentials, authorities);
+
+        logger.info("CustomAuthenticationToken: " + tenant);
         this.tenant = tenant;
         super.setAuthenticated(true); // must use super, as we override
     }
